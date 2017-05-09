fca38fa68a458576509dcf32e097394d6addfcb8
Andres Breton <bretonics@gmail.com>
2017-05-09 16:49:24 -0400

    rearrangement for stable version

--- Bioinformatics/NCBI.pm
+++ Bioinformatics/NCBI.pm
@@ -1,34 +1,64 @@
-package Eutil;
+package Bioinformatics::Eutil;
 
 use Exporter qw(import);
 our @ISA = qw(Exporter);
-our @EXPORT = qw(getNCBIfile); #functions exported by default
+our @EXPORT = qw(getNCBIfile);
+our @EXPORT_OK = qw();
 
 use warnings; use strict; use diagnostics; use feature qw(say);
 use Carp;
 use Bio::DB::EUtilities;
 
-# =============================================
-#
-# 	MASTERED BY: Andres Breton
-#	FILE: eutil.pm
-#
-# =============================================
+# Own Modules (https://github.com/bretonics/Modules)
+use MyConfig;
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# MAIN
+# ==============================================================================
+#
+#   CAPITAN:        Andres Breton, http://andresbreton.com
+#   FILE:           NCBI.pm
+#   LICENSE:
+#   USAGE:          Acces NCBI's E-utilities
+#   DEPENDENCIES:   - BioPerl modules
+#
+# ==============================================================================
+
+=head1 NAME
+
+Eutil - package interface for accessing NCBI's servers.
+
+=head1 SYNOPSIS
+
+use Bioinformatics::Eutil;
+
+=head1 DESCRIPTION
+
+This module was designed to utilize NCBi's E-utilities to access NCBI data.
+
+=head1 EXPORTS
+
+=head2 Default Behaviors
+
+use Bioinformatics::Eutil;
+
+Exports 'getNCBIfile' function by default.
+
+=head2 Optional Behaviors
+
+
+
+=head1 FUNCTIONS
 
 =head2 getNCBIfile
 
-    Arg [1]     : $ID, $outDir, $FORCE, $DATABASE, $TYPE, $email
+  Arg [1]     : $ID, $outDir, $FORCE, $DATABASE, $TYPE, $email
 
-    Example     : ($NCBIfile, $NCBIstatus) = getNCBIfile($id, $outDir, $FORCE, $DATABASE, $TYPE, $email);
+  Example     : ($NCBIfile, $NCBIstatus) = getNCBIfile($id, $outDir, $FORCE, $DATABASE, $TYPE, $email);
 
-    Description : Use NCBI's E-utilities to fetch file from server
+  Description : Use NCBI's E-utilities to fetch file from server
 
-    Returntype  : Scalar list (2)
+  Returntype  : Scalar list (2)
 
-    Status      : Stable
+  Status      : Stable
 
 =cut
 sub getNCBIfile {
@@ -52,4 +82,26 @@ sub getNCBIfile {
     sleep(3); #Don't overload NCBI requests
     return ($outFile, 1);
 }
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Andres Breton (C) 2017
+
+[LICENSE]
+
+=head1 CONTACT
+
+Please email comments or questions to Andres Breton, dev@andresbreton.com
+
+=head1 SETTING PATH
+
+If PERL5LIB was not set, do something like this:
+
+use FindBin; use lib "$FindBin::RealBin";
+
+This finds and uses current directoy as library location
+
+=cut
+
 1;
